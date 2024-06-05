3ee3f09395d783a1ea6742f7b11a1eef6a315e74
Wojciech Dlugosz <webmaster@styronet.pl>
2024-06-05 15:42:03 +0200

    Remove commented code

--- js/script.js
+++ js/script.js
@@ -47,31 +47,11 @@
 
     const computerMove = getMoveName(randomNumber);
 
-    /*if (randomNumber == 1) {
-  computerMove = "kamień";
-} else if (randomNumber == 2) {
-  computerMove = "papier";
-} else if (randomNumber == 3) {
-  computerMove = "nożyce";
-}*/
-
     printMessage("Mój ruch to (komputer): " + computerMove);
-
-    /*let playerInput = prompt(
-    "Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce."
-  );*/
 
     console.log("Gracz wpisał: " + playerInput);
 
     const playerMove = getMoveName(playerInput);
-
-    /*if (playerInput == "1") {
-  playerMove = "kamień";
-} else if (playerInput == 2) {
-  playerMove = "papier";
-} else if (playerInput == 3) {
-  playerMove = "nożyce";
-}*/
 
     printMessage("Twój ruch to (gracz): " + playerMove);
 
